8dab278fd26f99a6c22213224f5b3782a672a76a
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-02 08:28:40 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return: program should end with 0
+ */
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
+
