20e096e0df67f118f7d65cd6a96c8ffcbbac147e
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-02 12:15:50 -0700

    hello

---
+++ 0x00-hello_world/6-file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the string
+ * Return Always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Sizeof a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long init: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+	return (0);
+}
+
