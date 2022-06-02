1ed8488554a76caa90f083e278a665365a41a931
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-02 11:43:49 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints "Programming is like building a multilingual puzzle"
- * return: program should end with 0
+ *
+ * Return: Always (0) Success
  */
 int main(void)
 {
