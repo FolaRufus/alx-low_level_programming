98158bf29545639224cf542a55496ce8770e2300
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-02 20:52:00 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(longlongType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
         printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
 }
