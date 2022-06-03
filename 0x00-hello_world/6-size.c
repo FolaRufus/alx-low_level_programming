67a51702fb17d9db13ade9a844ae14333fee3717
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-02 21:10:41 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 /**
  * main Entry point
  *
+ * Description: using main function
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,12 +12,12 @@ int main(void)
 	long longType;
 	long long longlongType;
 	float floatType;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
         printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
-	
 	return (0);
 }
 
