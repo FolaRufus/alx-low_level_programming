12637e74ca3fb7a585624592e4c03f3c5a67e4a4
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-03 20:08:55 -0700

    variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,8 @@
 /*
 * main - prints all possible combinations of two-digit numbers
 * Description: using the main function
-* this program prints two-digits from 0-99,
+* this program prints two-digits from 0-99, separated by a comma
+* followed by a space.
 * Return: Always 0 (Success)
 */
 int main(void)
