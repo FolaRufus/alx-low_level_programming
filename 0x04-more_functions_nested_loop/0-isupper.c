9581f978d8b2ef9214c7478a6d1a4ed5ec0b0158
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-09 23:18:34 -0700

    functions

--- 0x04-more_functions_nested_loop/0-isupper.c
+++ 0x04-more_functions_nested_loop/0-isupper.c
@@ -1,16 +1,1 @@
-#include "main.h"
-
-/**
- * _isupper - main entry.
- * @c: integer to be checked.
- * Return: 1 if c is uppercase, 0 otherwise.
- */
-
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-
-	return (0);
-}
-
+#!/bin/bash
--- 0x04-more_functions_nested_loop/_putchar.c
+++ 0x04-more_functions_nested_loop/_putchar.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appopirately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#!/bin/bash
