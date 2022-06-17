f3c9ab095eeec9b071b8552ced135c7d20e1d9ef
FolaRufus <adesaanuafolasade@gmail.com>
2022-06-16 22:38:09 -0700

    arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,8 @@ char *cap_string(char *n)
 {
 	int i, x;
 	int cap = 32;
-	nt separators[] = {',', ';', '.', '?', '"','(', ')', '{', '}', ' ', '\n', '\t'};
+	int separators[] = {',', ';', '.', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t'};
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
